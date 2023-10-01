5791d2df7ea66fb63596f825a0022ebf23ae0652
Svyntsov Andriy <svincov_a@ukr.net>
2023-10-01 20:50:29 +0300

    2

--- middlewara/validation/contact-validation.js
+++
@@ -1,8 +0,0 @@
-import contactSchema from "../../schema/contact-schema.js";
-import { validateBody } from "../../decorators/index.js";
-
-const addContactValidate = validateBody(contactSchema.contactAddSchema);
-
-export default {
-    addContactValidate,
-}
