3cd2d36bee20e2af737f1e84a02ec88c367b6b9d
Svyntsov Andriy <svincov_a@ukr.net>
2023-10-03 03:01:53 +0300

    3

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 import mongoose from "mongoose";
 
-import app from "./app.js"
+import app from "./app.js";
 
-const DB_HOST = "mongodb+srv://Andriy:2008A@cluster0.5nx8ibg.mongodb.net/my-contacts?retryWrites=true&w=majority&appName=AtlasApp"
+const {DB_HOST, PORT = 3000} = process.env;
 
-mongoose.connect(DB_HOST)
-.then(() => {
-  app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
-})
-.catch(error => {
-  console.log(error.message);
-  process.exit(1);
-})
+
+mongoose
+  .connect(DB_HOST)
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// const DB_HOST = "mongodb+srv://Andriy:2008A@cluster0.5nx8ibg.mongodb.net/my-contacts?retryWrites=true&w=majority&appName=AtlasApp"
+
+// mongoose.connect(DB_HOST)
+// .then(() => {
+//   app.listen(3000, () => {
+//   console.log(`Server running. Use our API on port: ${PORT}`)
+// })
+// })
+// .catch(error => {
+//   console.log(error.message);
+//   process.exit(1);
+// })
 // 2008A
 
 
